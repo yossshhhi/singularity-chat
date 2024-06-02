4216485238a9cdda5600ae4c8293f52650fca69e
yossshhhi <arailim777@mail.ru>
2024-06-02 17:25:17 +0500

    added functions for sending personal messages, counting messages, displaying username

--- server/src/main/java/kz/yossshhhi/ClientHandler.java
+++ server/src/main/java/kz/yossshhhi/ClientHandler.java
@@ -11,6 +11,7 @@ public class ClientHandler {
     private DataOutputStream out;
     private Server server;
     private String username;
+    private int msgCounter;
 
     public ClientHandler(Server server, Socket socket) throws IOException {
         this.server = server;
@@ -23,15 +24,22 @@ public class ClientHandler {
                 while (true) {
                     String msg = in.readUTF();
                     if (msg.startsWith("/login ")) {
-                        username = msg.split("\\s+")[1];
-                        // /who_am_i
-                        sendMessage("/login_ok " + username);
-                        break;
+                        String input = msg.split("\\s+")[1];
+                        if (server.existsByUsername(input)) {
+                            sendMessage("/login_failed " + input);
+                        } else {
+                            username = input;
+                            sendMessage("/login_ok " + username);
+                            break;
+                        }
                     }
                 }
                 while (true) {
                     String msg = in.readUTF();
-                    server.broadcastMessage(username + ": " + msg);
+                    if (msgHandler(msg)) {
+                        server.broadcastMessage(username + ": " + msg);
+                        msgCounter++;
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -55,5 +63,36 @@ public class ClientHandler {
                 e.printStackTrace();
             }
         }
+    }
+    
+    private boolean msgHandler(String msg) throws IOException {
+        if (msg.startsWith("/p ")) {
+            privateMsgHandler(msg);
+            return false;
+        } else if (msg.equals("/who_am_i")) {
+            sendMessage("Ваше имя пользователя: " + username);
+            return false;
+        } else if (msg.equals("/stat")) {
+            sendMessage("Количество сообщений - " + msgCounter);
+            return false;
+        }
+        return true;
+    }
+
+    private void privateMsgHandler(String msg) throws IOException {
+        String[] split = msg.split("\\s+", 3);
+        if (split.length < 3) {
+            sendMessage("/bad_request Некорректный ввод данных");
+            return;
+        }
+        String recipient = split[1];
+        String message = split[2];
+        if (server.sendPrivateMessage(username, recipient, message)) {
+            sendMessage("/bad_request Пользователь не найден");
+        }
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
--- server/src/main/java/kz/yossshhhi/Server.java
+++ server/src/main/java/kz/yossshhhi/Server.java
@@ -5,10 +5,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Server {
     private int port;
     private List<ClientHandler> list;
+
     public Server(int port) {
         this.port = port;
         this.list = new ArrayList<>();
@@ -24,7 +26,7 @@ public class Server {
     }
 
     public void broadcastMessage(String msg) throws IOException {
-        for(ClientHandler clientHandler : list) {
+        for (ClientHandler clientHandler : list) {
             clientHandler.sendMessage(msg);
         }
     }
@@ -35,5 +37,19 @@ public class Server {
 
     public void unsubscribe(ClientHandler clientHandler) {
         list.remove(clientHandler);
+    }
+
+    public boolean existsByUsername(String username) {
+        return list.stream().anyMatch(clientHandler -> clientHandler.getUsername() != null && clientHandler.getUsername().equals(username));
+    }
+
+    public boolean sendPrivateMessage(String sender, String recipient, String msg) throws IOException {
+        Optional<ClientHandler> optional = list.stream().filter(clientHandler -> clientHandler.getUsername().equals(recipient)).findFirst();
+        if (optional.isPresent()) {
+            ClientHandler clientHandler = optional.get();
+            clientHandler.sendMessage("Сообщение от " + sender + ": " + msg);
+            return false;
+        }
+        return true;
     }
 }
